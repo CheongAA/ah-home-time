fa78e9d99096562c2a52bab2ab8f207ab9cb3c8b
cheonga <cheonga@neopin.io>
2025-05-23 18:52:56 +0900

    chore: next.config 변경

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = "ah-home-time";
-const path = isGithubPages ? `/${repoName}` : "";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: path,
-  assetPrefix: path,
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix: ".",
 };
 
 export default nextConfig;
